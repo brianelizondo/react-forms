88337ac4fdce828ee422dd60a27b9cb931019759
Brian Elizondo <brian_elizondo@msn.com>
2022-08-05 21:31:02 -0400

    React Forms Exercises => Further Study - When each Todo component is displayed, add a button with the text of 'Mark as completed'. When this button is clicked, add a strike through the text of the todo

--- src/Todo.js
+++ src/Todo.js
@@ -1,17 +1,24 @@
 import React from "react";
+import "./Todo.css"
 
-function Todo({ task, id, showEditForm, deleteTask }){
+function Todo({ task, id, showEditForm, completedTaskStatus, completedTask, deleteTask }){
     const handleEdit = () => {
         showEditForm({ id, task });
+    }
+    const handleCompleted = () => {
+        completedTask(id);
     }
     const handleDelete = () => {
         deleteTask(id);
     }
 
+    const classStyle = completedTaskStatus == false ? "Todo" : "Todo Todo-completed";
+
     return (
-        <div className="Todo">
+        <div className={classStyle}>
             { task }
-            <button onClick={handleEdit}>edit</button>
+            <button onClick={handleEdit}>Edit</button>
+            {!completedTaskStatus && <button onClick={handleCompleted}>Mark as completed</button>}
             <button onClick={handleDelete}>X</button>
         </div>
     );
--- src/TodoList.js
+++ src/TodoList.js
@@ -19,21 +19,27 @@ function TodoList(){
     };
 
     const addTask = task => {        
-        let newTask = { ...task, id: uuid() };
+        let newTask = { ...task, id: uuid(), completed: false };
         setTodos(tasks => [...tasks, newTask]);
     };
     const editTask = updateTask => {
         let updateTodos = todos.map(todo => {
             if(todo.id === updateTask.id){
-                return {
-                    ...todo,
-                    task: updateTask.task
-                }
+                return { ...todo, task: updateTask.task }
             }
             return {...todo}
         });
         setTodos(tasks => updateTodos);
         setEditTodo(EDIT_INITIAL_STATE);
+    };
+    const completedTask = idTask => {
+        let updateTodo = todos.map(todo => {
+            if(todo.id === idTask){
+                return { ...todo, completed: true }
+            }
+            return {...todo}
+        });
+        setTodos(tasks => updateTodo);
     };
     const deleteTask = idTask => {
         setTodos(todos.filter(todo => todo.id !== idTask));
@@ -44,7 +50,15 @@ function TodoList(){
             <NewTodoForm addTask={addTask} />
             <div className="TodoList-container">
                 { 
-                    todos.map(todo => <Todo task={todo.task} id={todo.id} key={todo.id} showEditForm={showEditForm} deleteTask={deleteTask} /> )
+                    todos.map(todo => <Todo 
+                        task={todo.task} 
+                        id={todo.id} 
+                        key={todo.id} 
+                        showEditForm={showEditForm} 
+                        completedTaskStatus={todo.completed} 
+                        completedTask={completedTask} 
+                        deleteTask={deleteTask} /> 
+                    )
                 }
                 {editTodo.show && <EditTodoForm id={editTodo.id} task={editTodo.task} editTask={editTask} />}
             </div>
